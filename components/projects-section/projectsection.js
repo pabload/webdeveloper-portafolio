3f1355d70ac0fccbea092b7af6dbab882d0eb671
unknown <juanpabload98@gmail.com>
2021-06-11 15:12:20 -0600

    se corrigio imagenes url

--- components/projects-section/projectsection.js
+++ components/projects-section/projectsection.js
@@ -18,24 +18,24 @@ const ProjectSection = () => {
                 </div>
                 <div className={`row g-0 justify-content-center align-items-center mb-3`}>
                     <div className={`col-lg-3 col-10`}>
-                        <ProjectCard projectName={'Colectivo Mistico'} ProjectImage={'/colectivomistico-project.PNG'} Stack={`React/Next.js/Ghost.js/Bootstrap`} description={`website for store Colectivo Mistico`} projectUrl={"https://colectivo-mistico-sitioweb.vercel.app/"} SourceCodeUrl={"https://github.com/pabload/colectivo-mistico-sitioweb"} />
+                        <ProjectCard projectName={'Colectivo Mistico'} ProjectImage={'/colectivomistico-project.png'} Stack={`React/Next.js/Ghost.js/Bootstrap`} description={`website for store Colectivo Mistico`} projectUrl={"https://colectivo-mistico-sitioweb.vercel.app/"} SourceCodeUrl={"https://github.com/pabload/colectivo-mistico-sitioweb"} />
                     </div>
                     <div className={`col-lg-3 col-10`}>
-                        <ProjectCard projectName={'Match pricer'} ProjectImage={'/matchpricer-project.PNG'} Stack={`React/Mongo/Express/Bootstrap`} description={`Tracker of prices`} projectUrl={"https://www.matchpricer.com/"} />
+                        <ProjectCard projectName={'Match pricer'} ProjectImage={'/matchpricer-project.png'} Stack={`React/Mongo/Express/Bootstrap`} description={`Tracker of prices`} projectUrl={"https://www.matchpricer.com/"} />
                     </div>
                     <div className={`col-lg-3 col-10`}>
-                        <ProjectCard projectName={'Sopa Numerica'} ProjectImage={'/sopanumerica-project.PGN'} Stack={`React/express/socket.io/Bootstrap`} description={`Multiplayer game for practice aritmetics`} projectUrl={"https://sopanumerica.netlify.app/"} />
+                        <ProjectCard projectName={'Sopa Numerica'} ProjectImage={'/sopanumerica-project.png'} Stack={`React/express/socket.io/Bootstrap`} description={`Multiplayer game for practice aritmetics`} projectUrl={"https://sopanumerica.netlify.app/"} />
                     </div>
                 </div>
                 <div className={`row g-0 justify-content-center align-items-center mb-3`}>
                     <div className={`col-lg-3 col-10`}>
-                        <ProjectCard projectName={'Cuidado con el perro'} ProjectImage={'/ecommerce-project.PGN'} Stack={`React/firebase/Bootstrap`} description={`Simultation of a web ecommerce using Paypal API`} projectUrl={"https://bd-ecommerce-ropa.firebaseapp.com/"} />
+                        <ProjectCard projectName={'Cuidado con el perro'} ProjectImage={'/ecommerce-project.png'} Stack={`React/firebase/Bootstrap`} description={`Simultation of a web ecommerce using Paypal API`} projectUrl={"https://bd-ecommerce-ropa.firebaseapp.com/"} />
                     </div>
                     <div className={`col-lg-3 col-10`}>
                         <ProjectCard projectName={'Personal portafolio'} ProjectImage={'/webportafolio-project.png'} Stack={`React/Next.js/Bootstrap`} description={`Personal portafolio using Next.js`} />
                     </div>
                     <div className={`col-lg-3 col-10`}>
-                         <ProjectCard projectName={'Salsa Tepin'} ProjectImage={'/salsatepin-project.PNG'} Stack={`HTML/CSS/vanilla Javascript`} description={`Website for Salsa Tepin`} projectUrl={"https://www.salsatepin.com.mx/"} />
+                         <ProjectCard projectName={'Salsa Tepin'} ProjectImage={'/salsatepin-project.png'} Stack={`HTML/CSS/vanilla Javascript`} description={`Website for Salsa Tepin`} projectUrl={"https://www.salsatepin.com.mx/"} />
                     </div>
                 </div>
             </div>
